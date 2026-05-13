25a83ed1ff0a5b1eeea6d0b5dd6dc293cf899487
Ashutosh Sanjeev Bhujbal <theflash257@gmail.com>
2026-05-13 04:07:48 +0100

    v2 update

--- src/components/Main/Sections/index.js
+++
@@ -1,9 +0,0 @@
-
-import Hero from "./Hero"
-import Projects from "./Projects/Projects"
-import About from "./About"
-import Contact from "./Contact"
-import Skills from "./Skills"
-// import Swiper from "./Swiper"
-
-export { Hero, Projects, About, Contact, Skills }
